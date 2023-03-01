d9ab5e4c8b33217de7a34985a2d632baa9159ad0
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2023-03-01 15:34:27 +0700

    update api_services.dart

--- app/lib/network/api_services.dart
+++ app/lib/network/api_services.dart
@@ -1,25 +1,12 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
 
 import 'dio_client.dart';
 import 'api_paths.dart';
-import '../models/product_detail.dart';
 
 class ApiServices {
   final DioClient dioClient;
 
   ApiServices({required this.dioClient});
-
-  // Future<ProductDetail> getProductDetail() async {
-  //   try {
-  //     final responseJson = await Dio().get(_baseHost + ApiPaths.getListProduct);
-  //     return ProductDetail.fromJson(responseJson);
-  //     // return ProductDetail.fromJson(responseJson);
-  //   } on SocketException {
-  //     rethrow;
-  //   }
-  // }
 
   Future<Response> getProductDetail() async {
     try {
