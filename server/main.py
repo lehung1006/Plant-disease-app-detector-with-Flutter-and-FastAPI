4e88e2b2947e0340960a041d67139277f2ac6392
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2023-05-04 20:50:00 +0700

    change https to http

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from config import settings
 if __name__ == "__main__":
-    uvicorn.run("app:app", host=settings.Host, port=settings.Port,  reload=True,
-                ssl_keyfile="./SSL/localhost+2-key.pem", 
-                ssl_certfile="./SSL/localhost+2.pem")
+    uvicorn.run("app:app", host=settings.Host, port=settings.Port,  reload=True)
+                #ssl_keyfile="./SSL/localhost+2-key.pem", 
+                #ssl_certfile="./SSL/localhost+2.pem")
